3c6e51d4c55b450e8cb09ba7d546e060796fdf15
Heuller <heullergit@gmail.com>
2021-06-10 21:10:05 -0300

    home adiantada

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,21 @@ class Home extends Component{
     render(){
         return(
             <section id="home">
-                <img src={Musculoso} alt="homem musculoso" className="musculoso"/>
+                <div id="frase">
+                    <span className="frase">STEP<br></br>INTO<br></br>GREAT-<br></br>NESS</span>
+                    <div id="subfrase">
+                        <span className="subfrase">FIND OUT EXACTLY WHAT DIET TRAINING IS BEST FOR YOU</span><br></br>
+                    </div>
+                    <span className="frase-calcular-dieta"><strong>DESEJA CALCULAR SUA DIETA?</strong></span><br></br>
+                    <div className="container-button">
+                        <button className="button-calcular">CALCULAR<i className="fas fa-times icone-button"></i></button>
+                    </div>
+                    
+                </div>
+                <div id="imagem">
+                    <img src={Musculoso} alt="homem musculoso" className="musculoso"/>
+                </div>
+                
             </section>
         );
     }
